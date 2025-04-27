51398df78e4f72ee4387a4219f9ddad5d8a35980
v4n3k <ivan71012@icloud.com>
2025-04-27 18:41:38 +0300

    feat(search): reset search query in header after navigation to unrelated pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import './App.module.css';
 import { AppRouter } from './AppRouter';
 import { Layout } from './layout/Layout';
+import { useSearchStore } from './stores/useSearchStore';
+import { RouteBase } from './types';
 
 const queryClient = new QueryClient();
 
 const App = () => {
+	const { pathname } = useLocation();
+	const setSearchQuery = useSearchStore(state => state.setSearchQuery);
+
+	useEffect(() => {
+		if (
+			!pathname?.includes(RouteBase.EXCURSION) &&
+			!pathname?.includes(RouteBase.SEARCHED_EXCURSIONS)
+		) {
+			setSearchQuery('');
+		}
+	}, [pathname]);
+
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Layout>
