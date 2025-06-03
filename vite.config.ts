f931a52eba142b6347fa5fde9c30907f3abea0fd
v4n3k <ivan71012@icloud.com>
2025-06-03 16:14:09 +0300

    feat: configure server for allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		port: 5173,
+		host: '127.0.0.1',
+		allowedHosts: [
+			'xn--90aciandhd9bwfddfe6f.xn--p1ai',
+			'www.xn--90aciandhd9bwfddfe6f.xn--p1ai',
+			'localhost',
+			'127.0.0.1',
+			'243.252.88.103',
+		],
+	},
 });
