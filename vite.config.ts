8ca5c7d1f3fdd231e1b236881b6d5168df710841
v4n3k <ivan71012@icloud.com>
2025-06-14 14:33:41 +0300

    config(vite): update allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
 		port: 5173,
 		host: '127.0.0.1',
 		allowedHosts: [
-			'xn--90aciandhd9bwfddfe6f.xn--p1ai',
-			'www.xn--90aciandhd9bwfddfe6f.xn--p1ai',
+			'xn----9sbelapeid5cyafedff1g.xn--p1ai/',
+			'www.xn----9sbelapeid5cyafedff1g.xn--p1ai/',
 			'localhost',
 			'127.0.0.1',
 			'243.252.88.103',
