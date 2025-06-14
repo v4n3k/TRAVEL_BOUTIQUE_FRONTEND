bf3c810a7658bd1c4d850266771aa66d720cdb1c
v4n3k <ivan71012@icloud.com>
2025-06-14 17:11:54 +0300

    config(api): change ip in prod mode

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const isDev = process.env.NODE_ENV === 'dev';
 
-const ip = isDev ? 'localhost' : '103.88.243.252';
+const ip = isDev ? 'localhost' : 'xn----9sbelapeid5cyafedff1g.xn--p1ai';
 const protocol = isDev ? 'http' : 'https';
 
 const baseURL = `${protocol}://${ip}:4200/api`;
