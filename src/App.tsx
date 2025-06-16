b28678cf4c059591b33044876ba2321972944375
v4n3k <ivan71012@icloud.com>
2025-06-16 14:02:42 +0300

    fix(css): replace initialization of css variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './App.module.css';
 import { AppRouter } from './AppRouter';
 import { Layout } from './layout/Layout';
 import { SearchQueryProvider } from './providers/SearchQueryProvider';
--- src/api/api.ts
+++ src/api/api.ts
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const isDev = process.env.NODE_ENV === 'dev';
 
-const ip = isDev ? 'localhost' : 'xn----9sbelapeid5cyafedff1g.xn--p1ai';
+const ip = isDev ? 'localhost:4200' : 'xn----9sbelapeid5cyafedff1g.xn--p1ai';
 const protocol = isDev ? 'http' : 'https';
 
-const baseURL = `${protocol}://${ip}:4200/api`;
+const baseURL = `${protocol}://${ip}/api`;
 
 export const api = axios.create({
 	baseURL,
