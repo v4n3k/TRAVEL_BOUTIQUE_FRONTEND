c195da62b198504639c1ad8e7e632c8ce702d84f
v4n3k <ivan71012@icloud.com>
2025-06-17 15:52:55 +0300

    fix(vite): change port to standard 5173 for dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
+const isDev = process.env.NODE_ENV === 'dev';
+
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		port: 443,
+		port: isDev ? 5173 : 443,
 		host: '127.0.0.1',
 		allowedHosts: [
 			'xn----9sbelapeid5cyafedff1g.xn--p1ai',
